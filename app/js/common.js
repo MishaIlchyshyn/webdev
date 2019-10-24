38a697d18e8dbc9a25d49d56b749178cbc7bec3a
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2019-10-24 14:42:31 +0300

    create section: header, sent-request, our-requirement + styles

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,15 @@
-$(function() {
+$(function () {
+	"use strict";
 
-	// Custom JS
+	$('.input').each(function () {
+		$(this).on('blur', function () {
+			if ($(this).val().trim() != "") {
+				$(this).addClass('has-val');
+			}
+			else {
+				$(this).removeClass('has-val');
+			}
+		})
+	})
 
 });
