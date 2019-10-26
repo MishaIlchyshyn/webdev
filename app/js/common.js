404e3f37e13fd1bf2b0129d816ef8fcf1f70569d
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2019-10-26 23:53:35 +0300

    code review and add map on page

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,4 @@
 $(function () {
-	"use strict";
 
 	$('.input').each(function () {
 		$(this).on('blur', function () {
@@ -13,3 +12,24 @@ $(function () {
 	})
 
 });
+
+$(function() {
+	$('a[href*="#"]').bind('click', function(e) {
+		e.preventDefault();
+
+		let target = $(this).attr("href");
+
+		$('html, body').stop().animate({ scrollTop: $(target).offset().top}, 500, function() {
+			location.hash = target;
+		});
+
+	return false;
+
+	});
+});
+
+let map = L.map('myMap').setView([49.839719, 24.023338], 17);
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+let marker = L.marker([49.839719, 24.023338]).addTo(map);
